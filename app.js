ee332682da319fc50b30b83681b8142338be2171
willie capre <wcap18@gmail.com>
2019-09-24 09:27:19 -0400

    Added pug files and eddited app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 const express = require ('express');
 const app = express();
-// middleware setting
+const myjson= require ('./data')
+
+//middleware
 app.set('view engine', 'pug');
 
 app.get('/', (req,res) => {
     res.render('index');
 });
-
-//setting routes
-app.get('/', (req,res) => {
-    res.render('index');
-});
-app.get('/', (req,res) => {
+app.get('/about', (req,res) => {
     res.render('about');
 });
 
-//error handler
+app.get('/project', (req,res) => {
+    res.render('project', { project: myjson});
+});
+//error handler for 404 error
 app.use((req, res, next) => {
-    console.log('');
+
     const err = new Error('Oh no, there has been a mistake!');
-    err.status = 500;
+    err.status = 404;
     next(err);
 });
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
-    res.render('error', err);
+    res.render('error');
 
 });
 
 
 
-
+//local server on port 3000
 app.listen(3000,() => {
-    console.log('The application is running on localhost:3000!')
+    console.log('The frontend server is running on localhost:3000!')
 });
